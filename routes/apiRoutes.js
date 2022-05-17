6b10c4042578f28296bab3479f8dacbca68da3df
Kelvin Chao <kelvinchao2011@gmail.com>
2022-05-16 20:12:31 -0700

    adding delete route but still buggy

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -7,18 +7,29 @@ router.get('/notes', (req, res) => {
   res.sendFile(path.join(__dirname, '../db/db.json'))
 })
 
-router.post('/', (req, res) => {
+router.post('/notes', (req, res) => {
   let db = JSON.parse(fs.readFileSync('./db/db.json'))
-  res.json(db)
 
   let newNote = {
     title: req.body.title,
     text: req.body.text,
     id: uuidv4()
   }
+
   db.push(newNote)
   fs.writeFileSync('./db/db.json', JSON.stringify(db))
   res.json(db)
+})
+
+router.delete('/notes/:id', (req, res) => {
+  let db = JSON.parse(fs.readFileSync('./db/db.json'))
+
+  let deleteNote = db.filter(array => {
+    array.id !== req.params.id
+  })
+
+  fs.writeFileSync('./db/db.json', JSON.stringify(deleteNote))
+  res.json(deleteNote)
 })
 
 module.exports = router
